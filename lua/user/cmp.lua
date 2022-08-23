be382b99d4fb8bd6ee76e620cdeec3aecb0c65c8
boboballu <tarun.835081@gmail.com>
2022-08-23 00:42:48 -0400

    added pyright startup in cmp.lua, but pyright cli fails with syntax error

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -68,3 +68,10 @@ cmp.setup {
 		native_menu = false,
 	},
 }
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['pyright'].setup {
+	capabilities = capabilities
+}
