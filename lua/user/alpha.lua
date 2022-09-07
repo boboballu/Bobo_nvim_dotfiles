cf92a9159f991c47935fbd8723d5443bc34429ca
boboballu <tarun.835081@gmail.com>
2022-09-07 01:19:36 -0400

    Fixed 'e' to open new file in alpha greeter

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -16,7 +16,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("`", "keymaps"),
 	dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "New file", ":e<CR>"),
+	dashboard.button("e", "New file", ":enew<CR>"),
 	dashboard.button("p", "Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
